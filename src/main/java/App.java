55ec6f602a35963e16235e95b037d90b59fd032d
Tomorenge <tomorenge3@gmail.com>
2019-04-17 14:56:26 +0300

    added index link

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,9 +19,13 @@ public class App {
 
         port(port);
 
-
-
         get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/squadForm", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
 
             model.put("template", "templates/squads-form.vtl");
